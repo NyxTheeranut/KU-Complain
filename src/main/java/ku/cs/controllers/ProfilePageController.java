12ae33f56436d851a6d01ff32e4d2123dec00f55
Nyx <theeranut.s@ku.th>
2022-08-07 22:05:58 +0700

    profile edit

--- src/main/java/ku/cs/controllers/ProfilePageController.java
+++ src/main/java/ku/cs/controllers/ProfilePageController.java
@@ -1,2 +1,17 @@
-package ku.cs.controllers;public class ProfilePageController {
+package ku.cs.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
+public class ProfilePageController {
+    @FXML
+    public void handleBackButton(ActionEvent actionEvent){
+        try{
+            com.github.saacsos.FXRouter.goTo("home_student");
+        } catch (IOException e){
+            System.err.println(e);
+        }
+    }
 }
