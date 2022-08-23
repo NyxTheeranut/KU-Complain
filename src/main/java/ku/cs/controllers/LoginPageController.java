a0d30dc011a3b12ea1aa80247be97da25acc6641
xastersphere <dujrawee.l@ku.th>
2022-08-23 23:53:51 +0700

    login register User class

--- src/main/java/ku/cs/controllers/LoginPageController.java
+++ src/main/java/ku/cs/controllers/LoginPageController.java
@@ -3,20 +3,47 @@ package ku.cs.controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import com.github.saacsos.FXRouter;
-
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
 
-public class LoginPageController {
 
-    @FXML public void handleLoginButton(ActionEvent actionEvent){
-        try{
-            FXRouter.goTo("home_student");
-        } catch (IOException e){
-            System.err.println("Error loading student menu page");
-            System.err.println(e);
+public class LoginPageController {
+        @FXML
+        private Button loginButton;
+        @FXML
+        private TextField username;
+        @FXML
+        private PasswordField password;
+        @FXML
+        private Label wrongLogin;
+
+    @FXML public void handleLoginButton(ActionEvent actionEvent) throws IOException {
+        checkLogin();
+    }
+
+    @FXML public void checkLogin() throws IOException {
+        if (username.getText().toString().equals("test") && password.getText().toString().equals("123")) {
+            try{
+                FXRouter.goTo("home_student");
+            } catch (IOException e) {
+                System.err.println("Error loading student menu page");
+                System.err.println(e);
+            }
+        }
+
+        else if (username.getText().toString().isEmpty() && password.getText().toString().isEmpty()) {
+            wrongLogin.setText("กรุณาระบุชื่อบัญชีและรหัสผ่าน");
+        }
+
+        else {
+            wrongLogin.setText("ชื่อบัญชีหรือรหัสผ่านไม่ถูกต้อง");
         }
     }
+
     @FXML public void handleRegisterButton(ActionEvent actionEvent) {
         try{
             FXRouter.goTo("register_page");
