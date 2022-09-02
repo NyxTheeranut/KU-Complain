e7b3f7e5b1b7e9f0617c076aed66164c2e799558
Nyx <theeranut.s@ku.th>
2022-09-03 00:10:32 +0700

    Profile picture

--- src/main/java/ku/cs/controllers/EditProfileController.java
+++ src/main/java/ku/cs/controllers/EditProfileController.java
@@ -3,19 +3,30 @@ package ku.cs.controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 
+import javax.swing.*;
+import java.io.File;
 import java.io.IOException;
 
 public class EditProfileController {
 
     @FXML private TextField reNameTextField;
-    @FXML
-    public void handleBackButton(ActionEvent actionEvent){
-        try{
-            com.github.saacsos.FXRouter.goTo("home_student");
-        } catch (IOException e){
-            System.err.println("Error loading login page");
-        }
+    @FXML private ImageView profilePicture;
+
+    @FXML public void handleProfilePicture(ActionEvent actionEvent){
+        selectPicture();
+    }
+
+    public void selectPicture(){
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Image Files", "*.png", "*jpg"));
+        File selectedFile = fileChooser.showOpenDialog(null);
+        Image image = new Image(selectedFile.toURI().toString());
+        profilePicture.setImage(image);
     }
 
 }
--- src/main/java/ku/cs/models/accounts/Moderator.java
+++ src/main/java/ku/cs/models/accounts/Moderator.java
@@ -1,4 +1,7 @@
 package ku.cs.models.accounts;
 
-public class Moderator {
+public class Moderator extends Account {
+    public Moderator(String name, String password) {
+        super(name, password);
+    }
 }
