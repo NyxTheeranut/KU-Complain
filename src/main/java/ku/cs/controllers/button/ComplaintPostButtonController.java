0e54abcdfdf546599ee5882017704477aa8c9621
Kan sriprapai <kan.sr@ku.th>
2022-09-09 21:46:52 +0700

    change Button from interface to class

--- src/main/java/ku/cs/controllers/button/ComplaintPostButtonController.java
+++ src/main/java/ku/cs/controllers/button/ComplaintPostButtonController.java
@@ -1,54 +1,12 @@
 package ku.cs.controllers.button;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.layout.HBox;
-import ku.cs.controllers.HomeController;
-import ku.cs.services.Button;
+import ku.cs.models.Button;
 
-import java.io.IOException;
-import java.util.ArrayList;
-
-public class ComplaintPostButtonController implements Button {
+public class ComplaintPostButtonController extends Button {
     @FXML
-    private HBox button;
-    @FXML
-    private Parent page;
-
-    @Override
-    public void loadPage() {
-        try{ //load complaint list page
-            page = FXMLLoader.load(getClass().getResource(packageStr + "page/" + "complaint_post.fxml"));
-        } catch (IOException e){
-            System.err.println("error loading complaint post page");
-        }
-        HomeController.loadPage(page);
-    }
-
-    @FXML
-    public void handleOnMouseEnterButton() {
-        button.getChildren().get(0).setStyle("-fx-text-fill: #ffffff");
-        button.getChildren().get(1).setStyle("-fx-text-fill: #ffffff");
-    }
-
-    public void handleOnMouseExitButton() {
-        if (button.getStyle().equals("-fx-background-color: #03a96b")) return;
-        button.getChildren().get(0).setStyle("-fx-text-fill: #9d9fa1");
-        button.getChildren().get(1).setStyle("-fx-text-fill: #9d9fa1");
-    }
-
-    public void handleOnMouseClickButton() {
-        button.setStyle("-fx-background-color: #03a96b");
-        loadPage();
-        ArrayList<Node> buttonList = HomeController.getAllButton();
-        for(Node i: buttonList){
-            i.setStyle("-fx-background-color: #2f3337");
-            ((HBox)i).getChildren().get(0).setStyle("-fx-text-fill: #9d9fa1");
-            ((HBox)i).getChildren().get(1).setStyle("-fx-text-fill: #9d9fa1");
-        }
-        button.setStyle("-fx-background-color: #03a96b");
-        handleOnMouseEnterButton();
+    public void initialize(){
+        pageName = "complaint_post.fxml";
+        setupFont();
     }
 }
