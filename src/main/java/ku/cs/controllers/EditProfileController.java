46ace50f2c0e243799489ac118fa972ce04b9200
Nyx <theeranut.s@ku.th>
2022-09-29 22:05:45 +0700

    change password

--- src/main/java/ku/cs/controllers/EditProfileController.java
+++ src/main/java/ku/cs/controllers/EditProfileController.java
@@ -2,33 +2,45 @@ package ku.cs.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
+import ku.cs.models.accounts.Account;
+import ku.cs.models.accounts.AccountList;
 import ku.cs.models.accounts.User;
+import ku.cs.services.datasource.DataSource;
+import ku.cs.services.datasource.accounts.AccountListFileDataSource;
+import ku.cs.services.searcher.SearchAccountByUserName;
+import ku.cs.util.Util;
 
-import java.io.File;
-import java.io.IOException;
+
+import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
-public class EditProfileController {
 
-    @FXML private TextField renameTextField;
+public class EditProfileController extends AccountList {
+
+    @FXML private Label usernameLabel;
+
+    @FXML private TextField rePasswordTextField;
     @FXML private ImageView profilePicture;
 
     @FXML public void handleProfilePicture(ActionEvent actionEvent) throws IOException {
         selectPicture();
     }
-    @FXML public void handleProfileName(ActionEvent actionEvent) throws IOException {
-        rename();
+    @FXML public void handleRePassword(ActionEvent actionEvent) throws IOException {
+       changePassword();
     }
 
     @FXML public void initialize(){
-        /* String url = getClass().getResource("" + imagePath).toExternalForm();
-        profilePicture.setImage(new Image(url));*/
+        usernameLabel.setText(Util.account.getName());
     }
 
     public void selectPicture() throws IOException {
@@ -39,19 +51,32 @@ public class EditProfileController {
         Image image = new Image(selectedFile.toURI().toString());
         profilePicture.setImage(image);
         Path from = Paths.get(selectedFile.toURI());
-        Path to = Paths.get("src\\main\\resources\\ku\\cs\\image\\" + selectedFile.getName());
+        Path to = Paths.get("data\\image\\profileImage\\" + selectedFile.getName());
         CopyOption[] options = new CopyOption[]{
                 StandardCopyOption.REPLACE_EXISTING,
                 StandardCopyOption.COPY_ATTRIBUTES};
         Files.copy(from.toFile().toPath(), to.toFile().toPath(),options);
+        DataSource<AccountList> dataSource = new AccountListFileDataSource();
+        AccountList accountList = dataSource.readData();
+        Account account = Util.search(Util.account.getName(),accountList.getAllAccount(),new SearchAccountByUserName());
+        accountList.changePicture(Util.account, selectedFile.getName());
+        account.setImagePath(selectedFile.getName());
 
     }
-    public void rename() throws IOException {
-        String name =renameTextField.getText();
-        final Path yourPath = Paths.get("src\\main\\resources\\ku\\cs\\data\\account_list.csv");
-        byte[] buff = Files.readAllBytes(yourPath);
-        String Name = new String(buff, Charset.defaultCharset());
-        Name = Name.replace(Pattern.quote("dujrawee"),name);
-        Files.write(yourPath, Name.getBytes());
+    public void changePassword(){
+        DataSource<AccountList> dataSource = new AccountListFileDataSource();
+        AccountList accountList = dataSource.readData();
+        Account account = Util.search(Util.account.getName(),accountList.getAllAccount(),new SearchAccountByUserName());
+        String newName = rePasswordTextField.getText();
+        accountList.changePassword(Util.account, newName);
+        dataSource.writeData(accountList);
+    }
+    @FXML
+    public void handleBackButton(ActionEvent actionEvent){
+        try{
+            com.github.saacsos.FXRouter.goTo("profile");
+        } catch (IOException e){
+            System.err.println("Error loading profile page");
+        }
     }
 }
--- src/main/java/ku/cs/controllers/ProfilePageController.java
+++ src/main/java/ku/cs/controllers/ProfilePageController.java
@@ -3,6 +3,7 @@ package ku.cs.controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import com.github.saacsos.FXRouter;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import ku.cs.models.accounts.Account;
@@ -11,12 +12,17 @@ import ku.cs.util.Util;
 import java.io.IOException;
 
 public class ProfilePageController {
+
+    @FXML private Label nameLabel;
     @FXML private ImageView image;
     Account account = Util.account;
+
+
     @FXML
     public void initialize(){
         String url = getClass().getResource("/ku/cs/image/"+account.getImagePath()).toExternalForm();
         image.setImage(new Image(url));
+        nameLabel.setText(account.getName());
     }
     @FXML
     public void handleEditProfileButton(ActionEvent actionEvent){
